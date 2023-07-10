087f6c19a35d7ec3c394453962a84271dea6ff94
Rohhelion <kirorei04@gmail.com>
2023-07-10 12:23:40 +0300

    deleted some files

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+// Fetch API request example
+fetch('https://api.example.com/books')
+  .then(response => response.json())
+  .then(data => {
+    // Handle the fetched data
+  })
+  .catch(error => {
+    // Handle any errors
+  });
+
+// Your code goes here
